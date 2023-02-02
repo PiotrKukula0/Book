a0e2528a151f7c8ced9c6874ca933b72b71f9990
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-02-02 17:29:50 +0100

    adding database readings given by axios

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Books = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books")
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchAllBooks()
+  }, [])
   return (
-    <div>Books</div>
+    <div>
+      <h1>Book Shop</h1>
+      <div className='books'>
+        {books.map(book => (
+          <div className='book' key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.description}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
